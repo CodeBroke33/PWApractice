658f2a653f5b3af6d055cf3dfbf8c9b9e9b02b78
Codebroke33 <minedom33@gmail.com>
2024-06-02 19:21:20 +0200

    update

---
+++ script.js
@@ -0,0 +1,30 @@
+// app.js
+
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    'styles.css',
+    'app.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache opened');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
